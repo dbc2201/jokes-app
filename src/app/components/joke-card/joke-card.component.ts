039ef29322be9fdf8b720b2b5f97bb208b3ca561
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-30 01:42:49 +0530

    added a joke field to joke card class

--- src/app/components/joke-card/joke-card.component.ts
+++ src/app/components/joke-card/joke-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Joke } from 'src/app/models/joke.model';
 
 @Component({
-  selector: 'app-joke-card',
-  templateUrl: './joke-card.component.html',
-  styleUrls: ['./joke-card.component.css']
+	selector: 'app-joke-card',
+	templateUrl: './joke-card.component.html',
+	styleUrls: ['./joke-card.component.css'],
 })
 export class JokeCardComponent {
+	@Input() joke: Joke;
 
+	constructor() {
+		this.joke = {
+			id: -1,
+			type: 'dummy',
+			setup: 'why does this joke fail?',
+			delivery: `it's a dummy`,
+		};
+	}
 }
