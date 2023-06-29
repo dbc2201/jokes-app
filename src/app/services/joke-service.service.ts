bbe09fc282f68137365d1994ca58ebcc0fd56db3
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-30 01:12:19 +0530

    added a function to return a random joke

--- src/app/services/joke-service.service.ts
+++ src/app/services/joke-service.service.ts
@@ -6,4 +6,14 @@ import { Injectable } from '@angular/core';
 })
 export class JokeService {
 	constructor(private httpService: HttpClient) {}
+
+	/**
+	 * this function call the Random Joke API to get a random Joke
+	 * @returns a random joke
+	 */
+	getRandomJoke() {
+		return this.httpService.get(
+			'https://official-joke-api.appspot.com/random_joke'
+		);
+	}
 }
