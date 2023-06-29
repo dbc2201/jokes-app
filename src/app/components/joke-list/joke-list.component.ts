6277a06d810ab84b741633017084432cc308c6c5
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-30 01:02:50 +0530

    added a joke array for joke list manager component

--- src/app/components/joke-list/joke-list.component.ts
+++ src/app/components/joke-list/joke-list.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Joke } from 'src/app/models/joke.model';
 
 @Component({
-  selector: 'app-joke-list',
-  templateUrl: './joke-list.component.html',
-  styleUrls: ['./joke-list.component.css']
+	selector: 'app-joke-list',
+	templateUrl: './joke-list.component.html',
+	styleUrls: ['./joke-list.component.css'],
 })
 export class JokeListComponent {
+	private jokes: Joke[];
 
+	constructor() {
+		this.jokes = [
+			{
+				id: 1,
+				type: 'general',
+				setup: 'dummy setup',
+				punchline: 'punchline',
+			},
+			{
+				id: 2,
+				type: 'general',
+				setup: 'dummy setup',
+				punchline: 'punchline',
+			},
+			{
+				id: 3,
+				type: 'general',
+				setup: 'dummy setup',
+				punchline: 'punchline',
+			},
+			{
+				id: 4,
+				type: 'general',
+				setup: 'dummy setup',
+				punchline: 'punchline',
+			},
+		];
+	}
 }
