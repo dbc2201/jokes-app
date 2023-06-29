9e84e50ffebeee5fe0d67dc3a8fc29eab6e22140
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-30 02:15:08 +0530

    created a joke array

--- src/app/components/joke-list-manager/joke-list-manager.component.ts
+++ src/app/components/joke-list-manager/joke-list-manager.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Joke } from 'src/app/models/joke.model';
 
 @Component({
-  selector: 'app-joke-list-manager',
-  templateUrl: './joke-list-manager.component.html',
-  styleUrls: ['./joke-list-manager.component.css']
+	selector: 'app-joke-list-manager',
+	templateUrl: './joke-list-manager.component.html',
+	styleUrls: ['./joke-list-manager.component.css'],
 })
 export class JokeListManagerComponent {
+	jokes: Joke[];
 
+	constructor() {
+		this.jokes = [];
+	}
 }
