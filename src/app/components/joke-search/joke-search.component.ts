d855b52a634e8127ac7cda6389355610864e75fc
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-30 01:59:43 +0530

    added functions to be called later

--- src/app/components/joke-search/joke-search.component.ts
+++ src/app/components/joke-search/joke-search.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-joke-search',
-  templateUrl: './joke-search.component.html',
-  styleUrls: ['./joke-search.component.css']
+	selector: 'app-joke-search',
+	templateUrl: './joke-search.component.html',
+	styleUrls: ['./joke-search.component.css'],
 })
 export class JokeSearchComponent {
+	searchJoke(): void {}
 
+	resetSearchBar(): void {}
 }
