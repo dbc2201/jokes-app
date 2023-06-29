bd87a2b28c32dcfa45ca8d01af5911699a2f8c5f
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-30 00:25:43 +0530

    import form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { HeaderComponent } from './components/header/header.component';
+import { JokeListManagerComponent } from './components/joke-list-manager/joke-list-manager.component';
+import { JokeSearchComponent } from './components/joke-search/joke-search.component';
+import { JokeListComponent } from './components/joke-list/joke-list.component';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { JokeListManagerComponent } from './components/joke-list-manager/joke-list-manager.component';
-import { JokeSearchComponent } from './components/joke-search/joke-search.component';
-import { JokeListComponent } from './components/joke-list/joke-list.component';
 import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [
@@ -34,6 +36,7 @@ import { MatInputModule } from '@angular/material/input';
 		MatListModule,
 		MatButtonModule,
 		MatInputModule,
+		FormsModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
