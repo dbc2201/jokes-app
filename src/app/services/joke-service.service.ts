8ba81fc9a6e3a4ccee257d0de1eac0706f03eec2
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-06-30 02:39:44 +0530

    added function to get 10 random 'safe' jokes

--- src/app/services/joke-service.service.ts
+++ src/app/services/joke-service.service.ts
@@ -15,7 +15,17 @@ export class JokeService {
 	 */
 	getRandomJoke(): Observable<Joke> {
 		return this.httpService.get<Joke>(
-			' https://v2.jokeapi.dev/joke/programming?type=twopart'
+			'https://v2.jokeapi.dev/joke/programming?type=twopart'
+		);
+	}
+
+	/**
+	 * this function call the Random Joke API to get 10 random Jokes
+	 * @returns an 'observable' of an array of the 'Joke' type that contains a random joke
+	 */
+	getTenRandomJokes(): Observable<Array<Joke>> {
+		return this.httpService.get<Array<Joke>>(
+			' https://v2.jokeapi.dev/joke/programming?type=twopart&safe-mode&amount=10'
 		);
 	}
 }
